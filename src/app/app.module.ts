4eb13d6d4a8ffbed7d7cb9170283e0d476d08e95
victoria mwangi <victoriamwangi021@gmail.com>
2022-04-08 13:32:14 +0300

    feat: form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FormsModule } from '@angular/forms';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { TimeCountPipe } from './time-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteFormComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
