ac1a232ee60e82b1be39448b5df49ee62f92ccea
victoria mwangi <victoriamwangi021@gmail.com>
2022-04-11 07:16:05 +0300

    feat:calculated days since a quote was posted

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showName : boolean;
-constructor(public id: number, public name: string, public authorName: string, public message: string){
+constructor(public id: number, public name: string, public authorName: string, public message: string,public fullDate: Date){
   this.showName = false;
 }
 }
--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -1,12 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+
 @Pipe({
   name: 'timeCount'
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let nowTime:any = new Date(today.getFullYear(), (today.getMonth())+1, today.getDate());
+    alert(nowTime)
+    var timeDifference  = Math.abs(value - nowTime);
+    var secondCounter =  (timeDifference * 0.001)/ 86400
+
+    if(secondCounter >= 1){
+      return secondCounter;
+    }else{
+      return 0;
+    }
+
   }
 
 }
